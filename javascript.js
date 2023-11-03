2df3f384fb8b0836e5c57809681ac6760b1657ba
GianniCbd <giannicabiddu@gmail.com>
2023-11-03 15:38:15 +0100

    U2-W2-PROGETTO SETTIMANALE

--- javascript.js
+++ javascript.js
@@ -1,1 +1,4 @@
-
+const autoEp1 = document.getElementById("autoEp1");
+const autoEp2 = document.getElementById("autoEp2");
+const saveButton = document.getElementById("saveButton");
+const deleteButton = document.getElementById("deleteButton");
